759e4f86bade2e37a1a29efcf22a018b879dd88f
vgomtej353 <vgomtej353@g.educaand.es>
2023-10-07 16:59:38 +0200

    Demasiada pantalla para darle vueltas al 16

---
+++ ejerciciosU1/Ejercicio16.py
@@ -0,0 +1,5 @@
+
+barrapan = float(3.49)
+
+barravendida = print (int(input('Cuantas barras pasadas has vendido:')))
+print('El precio habitual de la barra fresca es: '(barrapan)), '$', ('El descuento que se le aplica es de: ', float(barrapan - (barrapan * 0.6))), '$', (barravendida * barrapan), '$'
