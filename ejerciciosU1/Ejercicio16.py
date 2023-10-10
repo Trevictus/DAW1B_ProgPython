e40ead24209d4d122a709a0f772e0fc56055a4ad
vgomtej353 <vgomtej353@g.educaand.es>
2023-10-10 13:09:57 +0200

    Subiendo los últimos ejercicios

--- ejerciciosU1/Ejercicio16.py
+++ ejerciciosU1/Ejercicio16.py
@@ -1,9 +1,9 @@
 
 barrapan = float(3.49)
-barravendida = barrapan * 0.6 - barrapan
+barravendida = barrapan - barrapan * 0.6
 
 unidtotales = float(input('¿Cuantas barras pasadas has vendido?:'))
 
 print('El precio habitual de una barra de pan es de: ', barrapan)
-print('El descuento que se le hace por no ser fresca es de: ', barrapan - barravendida)
+print('El descuento que se le hace por no ser fresca es de: ', barrapan - barrapan * 0.6)
 print('El coste total de todas las barras no frescas es de: ',barravendida * unidtotales)
